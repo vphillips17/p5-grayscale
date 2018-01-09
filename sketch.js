8089fb38defcbd946028b8d8b53c1b5eeb145633
VictorP <vphillips@wlapcs.org>
2018-01-09 20:26:07 +0000

    built grayscale demo

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup(){
+    createCanvas(600,400);
+    background(255);
+    noStroke();
+    num =0;
+    frameRate(10);
+}
+
+
+function draw(){
+    if(num < 256){
+        fill(num);
+        ellipse(width/2, height/2, 250, 250);
+        
+        fill('#FF0000');
+        textSize;
+        textAlign(CENTER, CENTER);
+        text(num, width/2, height/2);
+        
+        num = num + 1;
+    }
+}                                     
